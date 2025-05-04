34f6b0675a0f60d2868d13c9534e0228fddafa66
RyutoLBX <nrsutedjo@gmail.com>
2025-05-04 17:37:42 +0700

    Add clock and circleshape.py

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ from constants import *
 
 def main() -> None:
   print("Starting Asteroids!")
-  # print(f"Screen width: {SCREEN_WIDTH}")
-  # print(f"Screen height: {SCREEN_HEIGHT}")
+
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  # Clock definition
+  cl = pygame.time.Clock()
+  dt = 0.0
+
+  # Game Loop
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
     screen.fill("#000000")
     pygame.display.flip()
+    dt = cl.tick(60)/1000
 
 if __name__ == "__main__":
   main()
